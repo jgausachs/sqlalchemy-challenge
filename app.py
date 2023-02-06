f5ac49cd7d93f64d62bf264d2c35de7487b17c17
jgausachs <jgausachs@yahoo.com>
2023-02-06 20:01:06 +1100

    Further advanced Flask implementation

--- app.py
+++ app.py
@@ -60,6 +60,71 @@ def precipitation():
     # Retun the JSON representation of the precitation_res dictionary
     return jsonify(precipitation_res)
 
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a JSON list of stations from the dataset"""
+
+    # Session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all stations from Station table
+    results = session.query(Station.station).all()
+    session.close()
+
+    # Convert list of tuples into normal list
+    stations = list(np.ravel(results))
+
+    # Return the JSON representation of the stations list
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def temps():
+    """Return a JSON list of temperature observations for the previous year"""
+
+    # Session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query the dates and temperature observations of the most active station for the last year of data
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= '2016-08-23').all()
+    session.close()
+
+    # Convert list of tuples into normal list
+    temps = list(np.ravel(results))
+
+    # Return the JSON representation of the temps list
+    return jsonify(temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    """Return a JSON list of the minimum temperature, the average temperature,"""
+    """and the maximum temperature for a specified start or start-end range"""
+
+    # Session (link) from Python to the DB
+    session = Session(engine)
+
+    # Logic when start date only given
+    if not end:
+        results = session.query(\
+            func.min(Measurement.tobs),\
+            func.avg(Measurement.tobs),\
+            func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    # Logic when start and end dates give
+    results = session.query(\
+        func.min(Measurement.tobs),\
+        func.avg(Measurement.tobs),\
+        func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
